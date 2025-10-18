b11240faae5422973a04976e223393574c911519
ErfanKashef <129071476+ErfanKashef@users.noreply.github.com>
2025-10-18 11:50:20 +0330

    Feat:add folder home

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -1,21 +1,21 @@
-'use client';
+"use client";
 
-import Header from '@/components/header';
-import Hero from '@/components/hero';
-import TrustedBrands from '@/components/trusted-brands';
-import HowItWorks from '@/components/how-it-works';
-import Features from '@/components/features';
-import Testimonials from '@/components/testimonails';
-import Pricing from '@/components/pricing';
-import FAQ from '@/components/faq';
-import CallToAction from '@/components/call-to-action';
-import Contact from '@/components/contact';
-import Footer from '@/components/footer';
+import Header from "@/components/header";
+import Hero from "@/components/hero";
+import TrustedBrands from "@/components/trusted-brands";
+import HowItWorks from "@/components/how-it-works";
+import Features from "@/components/features";
+import Testimonials from "@/components/testimonails";
+import Pricing from "@/components/pricing";
+import FAQ from "@/components/faq";
+import CallToAction from "@/components/call-to-action";
+import Contact from "@/components/contact";
+import Footer from "@/components/footer";
 
 export default function Page() {
-	return (
-		<div className="min-h-screen">
-			<Header />
+  return (
+    <div className="min-h-screen">
+      <Header />
       <Hero />
       <TrustedBrands />
       <HowItWorks />
@@ -26,6 +26,6 @@ export default function Page() {
       <CallToAction />
       <Contact />
       <Footer />
-		</div>		
-	);
+    </div>
+  );
 }
---
+++ app/home/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import Header from "@/components/header";
+
+const Home = () => {
+  return (
+    <div className="container mx-auto">
+      <Header />
+      <div></div>
+    </div>
+  );
+};
+
+export default Home;
